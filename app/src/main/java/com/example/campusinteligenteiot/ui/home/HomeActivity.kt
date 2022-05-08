3610bb471511a1a79230631fc506c357526b9fe5
Cristian1711 <i82goorc@uco.es>
2022-05-08 17:05:29 +0200

    imagen en mapa

--- app/src/main/java/com/example/campusinteligenteiot/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/campusinteligenteiot/ui/home/HomeActivity.kt
@@ -3,11 +3,15 @@ package com.example.campusinteligenteiot.ui.home
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,17 +19,28 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import com.example.campusinteligenteiot.R
+import com.example.campusinteligenteiot.api.model.UserProvider
+import com.example.campusinteligenteiot.api.model.UsersResponse
 import com.example.campusinteligenteiot.databinding.ActivityHomeBinding
 import com.example.campusinteligenteiot.ui.home.car.CarFragment
+import com.example.campusinteligenteiot.ui.home.main.MainHomeViewModel
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.nav_header_drawer.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.security.AccessController.getContext
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var navController: NavController
+    private lateinit var user: UsersResponse
+    private val viewModel by viewModels<MainHomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,14 +68,38 @@ class HomeActivity : AppCompatActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
 
-        binding.navigationView.getHeaderView(0).imageView
-        binding.navigationView.getHeaderView(0).textView2
+        viewModel.onCreate()
+        viewModel.resultUsers.observe(this, Observer {
+
+            UserProvider.users = viewModel.resultUsers.value!!
+            println("Lista de usuarios")
+            println(UserProvider.users)
+            GlobalScope.launch(Dispatchers.Main) {
+                user = viewModel.getUserFromLocal(viewModel.getId())
+                println("DATOS USUARIO DE BASE DE DATOS LOCAL")
+                println(user.name)
+                println(user.collegeDegree)
+                println(user.surname)
+                val media = user.profileImage
+                val storageReference = FirebaseStorage.getInstance()
+                val gsReference = storageReference.getReferenceFromUrl(media!!)
+                gsReference.downloadUrl.addOnSuccessListener {
+                    Glide.with(this@HomeActivity).load(it).into(binding.navigationView.getHeaderView(0).imageView)
+                    binding.navigationView.getHeaderView(0).textView2.setText(user.name)
+                }
+            }
+        })
+
+
+
 
 
         //setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
+
+
 
 
 
--- app/src/main/java/com/example/campusinteligenteiot/ui/home/main/MainHomeFragment.kt
+++ app/src/main/java/com/example/campusinteligenteiot/ui/home/main/MainHomeFragment.kt
@@ -5,10 +5,8 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +15,7 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -126,7 +125,17 @@ class MainHomeFragment : Fragment(), OnMapReadyCallback, MapboxMap.OnMapClickLis
                 createUserGetStream(user)
             }
         })
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            showProfileImage()
+        }, 2000)
+
         return binding.root
+    }
+
+    private fun showProfileImage() {
+        binding.profileImageLoading.isVisible = false
+        binding.profileImage.isVisible = true
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
