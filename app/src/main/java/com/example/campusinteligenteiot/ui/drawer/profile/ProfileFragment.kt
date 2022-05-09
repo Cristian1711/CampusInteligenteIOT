6713fe6352f43461af5ec6fb1f61d7e7b1325885
Cristian1711 <i82goorc@uco.es>
2022-05-09 02:03:12 +0200

    carga perfil

--- app/src/main/java/com/example/campusinteligenteiot/ui/drawer/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/campusinteligenteiot/ui/drawer/profile/ProfileFragment.kt
@@ -9,8 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.campusinteligenteiot.R
 import com.example.campusinteligenteiot.databinding.ProfileFragmentBinding
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.item_collegedegree.view.*
+import kotlinx.android.synthetic.main.item_image.view.*
+import kotlinx.android.synthetic.main.nav_header_drawer.view.*
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment : Fragment() {
 
@@ -25,7 +31,33 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = ProfileFragmentBinding.inflate(inflater,container,false)
+
+        val sharedPreferences = requireContext().getSharedPreferences("MY_PREF", Context.MODE_PRIVATE)
+
+        val media = sharedPreferences?.getString("user_profileimage", "null")
+
+        loadImage(media)
+
+        binding.itemImage.username.text = sharedPreferences?.getString("user_username", "null")
+
+        binding.itemImage.description.text = sharedPreferences?.getString("user_description", "null")
+
+        binding.itemEdit.name.text = (sharedPreferences?.getString("user_name", "null")
+                + " " + sharedPreferences?.getString("user_surname", "null"))
+
+        binding.itemCollegedegree.collegeDegree.text = sharedPreferences?.getString("user_collegeDegree", "null")
+
+        binding.itemEmail.email.text = sharedPreferences?.getString("user_email", "null")
+
         return binding.root
+    }
+
+    private fun loadImage(media: String?) {
+        val storageReference = FirebaseStorage.getInstance()
+        val gsReference = storageReference.getReferenceFromUrl(media!!)
+        gsReference.downloadUrl.addOnSuccessListener {
+            Glide.with(this).load(it).into(binding.itemImage.image2)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,11 +67,6 @@ class ProfileFragment : Fragment() {
             findNavController().navigate(
                 R.id.action_profileFragment_to_configFragment)
         }
-
-        val sharedPreferences = requireContext().getSharedPreferences("MY_PREF", Context.MODE_PRIVATE)
-        val user_id = sharedPreferences?.getString("user_id", "null")
-        println("EL ID DEL USUARIO ACTUAL ES")
-        println(user_id)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
