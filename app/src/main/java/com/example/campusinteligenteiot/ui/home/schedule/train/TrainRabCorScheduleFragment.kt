3f4f4feac86c49fda681c9022d70febb471409c5
Cristian1711 <i82goorc@uco.es>
2022-08-07 21:26:05 +0200

    edicion y proximas salidas modulo horario

--- app/src/main/java/com/example/campusinteligenteiot/ui/home/schedule/train/TrainRabCorScheduleFragment.kt
+++ app/src/main/java/com/example/campusinteligenteiot/ui/home/schedule/train/TrainRabCorScheduleFragment.kt
@@ -1,6 +1,7 @@
 package com.example.campusinteligenteiot.ui.home.schedule.train
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +14,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import com.example.campusinteligenteiot.databinding.FragmentTrainRabCorScheduleBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TrainRabCorScheduleFragment : Fragment() {
     private  var _binding: FragmentTrainRabCorScheduleBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<TrainScheduleViewModel>()
     private val SCHEDULE_ID = "RabanalesCordoba"
+    private var departuresInSeconds: Array<Long> = Array(18) {1}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,11 +46,86 @@ class TrainRabCorScheduleFragment : Fragment() {
                 schedule?.departures,
                 schedule?.arrivals
             )
-
+            departuresInSeconds = transformDeparturesToSecond(fullSchedule[0])
+            val remainingTime = calculateNextDeparture(departuresInSeconds)
+            println("Quedan: $remainingTime segundos para la proxima salida")
+            playChrono(remainingTime)
+            fillTitles()
             fillTableLayout(fullSchedule)
         }
 
     }
+
+    private fun playChrono(remainingTime: Long) {
+        if(remainingTime == 0L){
+            binding.chronometerText.text = "No hay próximas salidas"
+            binding.chronometer.text = "Espere al día siguiente"
+        }
+        else {
+
+            var remainingTimeMiliSeconds = remainingTime * 1000
+            object : CountDownTimer(remainingTimeMiliSeconds, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    var timeInSeconds = (millisUntilFinished / 1000).toInt() + 1
+                    val hours = timeInSeconds / 3600
+                    timeInSeconds = timeInSeconds % 3600
+                    val minutes = timeInSeconds / 60
+                    timeInSeconds = timeInSeconds % 60
+
+                    binding.chronometer.text =
+                        hours.toString().padStart(2, '0') + ":" + minutes.toString()
+                            .padStart(2, '0') + ":" + timeInSeconds.toString().padStart(2, '0')
+                }
+
+                override fun onFinish() {
+                    calculateNextDeparture(departuresInSeconds)
+                }
+
+            }.start()
+        }
+
+    }
+
+    private fun transformDeparturesToSecond(departures: java.util.ArrayList<String>?): Array<Long> {
+        val departuresInSeconds: Array<Long> = Array(departures!!.size) { 1 }
+
+        for(i in (0 until departures.size)){
+            val parts = departures[i].split(":")
+            departuresInSeconds[i] = ((parts[0].toLong())*3600 + (parts[1].toLong())*60)
+        }
+
+        return departuresInSeconds
+    }
+
+    private fun calculateNextDeparture(departures: Array<Long>): Long {
+        var remainingTime: Long = 0
+        val cal: Calendar = Calendar.getInstance()
+        val seconds: Int = cal.get(Calendar.SECOND)
+        val minutes: Int = cal.get(Calendar.MINUTE)
+        val hour = cal.get(Calendar.HOUR_OF_DAY);
+        val timeInSeconds = hour*3600 + minutes*60 + seconds
+
+        for(i in (departures.indices)){
+            println("Segundos actuales: $timeInSeconds")
+            println("Tiempo en segundos salida: " + departures[i])
+            if(timeInSeconds < departures[i]){
+                remainingTime = departures[i+1] - timeInSeconds
+                println("Diferencia: $remainingTime")
+                break
+            }
+        }
+        return remainingTime
+    }
+
+    private fun fillTitles() {
+        val titles = LayoutInflater.from(context).inflate(R.layout.row_table_edit_title, null, false)
+        val departures = titles.findViewById<View>(R.id.departures) as TextView
+        val arrivals = titles.findViewById<View>(R.id.arrivals) as TextView
+        departures.text = getString(R.string.departures)
+        arrivals.text = getString(R.string.arrivals)
+        binding.tlTabla.addView(titles)
+    }
+
 
     private fun fillTableLayout(fullSchedule: Array<ArrayList<String>?>){
         for(i in (0 until fullSchedule[0]!!.size)){
